3701168864713c709224de9954df746d352803f6
Shreyabhaskar <shreyabhaskar1991@gmail.com>
2026-06-01 21:49:17 +0530

    Add C# learning projects

---
+++ MyProject/MyProject/Program.cs
@@ -0,0 +1,15 @@
+﻿//declare a variable
+//declaring a string variable
+//variable are used to store data inside of the memory and allows you to resuse it inside of your code
+
+string myFriendName;
+//Assign
+myFriendName = "Shreya";
+//run
+Console.WriteLine(myFriendName);
+//decalring and assigning
+string myFriendsName2 = "frank";
+//overwriting
+myFriendName = "Vivin";
+Console.WriteLine(myFriendName);
+Console.ReadKey();
