6929c234f056d2fdc3dcc0c50eb3adbdfc1413b9
Shreyabhaskar <shreyabhaskar1991@gmail.com>
2026-06-01 22:21:46 +0530

    Example

---
+++ AdditionCalc/AdditionCalc/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("enter something"); //writeline method, basically prints
+
+//read line is method..method is collection of lines of code that will be executed once you call the method
+
+//variable ->userInput
+//datatype -> string
+string userInput = Console.ReadLine();
+//takes the user i/p and stores it
+Console.WriteLine("You enetered " + userInput);
+Console.ReadKey(); //console does not shuts down automatically
